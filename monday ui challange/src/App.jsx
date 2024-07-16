ccac6d5e1a4e109551aee5a2f2083dad3a500de9
theCalculatar <alpheusm13@gmail.com>
2024-07-16 09:32:30 +0200

    Flexbox thingie folder added

---
+++ monday ui challange/src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex-items'>
+      <div className='row-1'>
+        <div className='item-1'></div>
+        <div className='item-4'></div>
+        <div className='item-1'></div>
+      </div>
+      <div className='row-2'>
+        <div className='item-3'></div>
+        <div className='item-2'></div>
+        <div className='item-3'></div>
+      </div>
+      <div className='row-3'>
+        <div className='item-1'></div>
+        <div className='item-4'></div>
+        <div className='item-1'></div>
+      </div>
+
+      {/* <ul className='list'>
+        <li> 
+            <a className='a_link' href="phifg">hmmm</a>
+        </li>
+        <li> 
+            <a className='a_link' href="phifg">hmmm</a>
+        </li>
+        <li> 
+            <a className='a_link' href="phifg">hmmm</a>
+        </li>
+        <li> 
+            <a className='a_link' href="phifg">hmmm</a>
+        </li>
+        <li> 
+            <a className='a_link' href="phifg">hmmm</a>
+        </li>
+      </ul> */}
+
+    </div>
+  )
+}
+
+export default App

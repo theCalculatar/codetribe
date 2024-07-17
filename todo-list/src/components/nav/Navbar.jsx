e09b56b8674d3662684744289a3cd0c72825f5c4
theCalculatar <alpheusm13@gmail.com>
2024-07-17 23:48:14 +0200

    added search

--- todo-list/src/components/nav/Navbar.jsx
+++ todo-list/src/components/nav/Navbar.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './navbar.module.css'
 import { TfiWorld } from "react-icons/tfi";
 import { FaArrowRightLong } from "react-icons/fa6";
+import Search from '../search/Search';
+import { useLocation } from 'react-router-dom';
 
-function Navbar() {
+function Navbar(props,{todos,home}) {
+  // const home = props.home
+  const [isLoggenIn,setLogin] = useState(false)
+  const location = useLocation()
+
   return (
-    <div className={style.navbar}>
+    <div className={style.navbar} style={{}}>
         <div className={style.col_1}>
             <h2>inotes</h2>
             <hr />
             <a href='mailto:info@inotes.com'>info@inotes.com <FaArrowRightLong/></a>
         </div>
+        {isLoggenIn && <Search todos={todos}/>}
         <div className={style.row_1}>
             <ul>
                 <li><a className={style.yyyy} href=""><TfiWorld/></a></li>
-                <li><a href="">Sign up</a></li>
+                {!isLoggenIn&&<li>{location.pathname=='/login'?<a href="/register">Sign up</a>:<a href="login">Sign in</a>}</li>}
                 <li><a id={style.demo} href="">Request Demo </a></li>
             </ul>
         </div>
---
+++ todo-list/src/components/search/Search.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+// import './search.module.css'
+import styles from './search.module.css'
+function Search() {
+    const [inputText, setInputText] = useState('')
+    const inputHandler = (e) =>{
+        let lowerCase = e.target.value.toLowerCase()
+        setInputText(lowerCase)
+    }
+    const [todos,setTodos] = useState( ()=>{ 
+        const local = localStorage.getItem('todos')
+        return local?JSON.parse(local):[]}
+    )
+
+    const datafiltered = todos.filter((el) => {
+        //if no input the return the original
+        if (inputText === '') {
+            // return []
+        }
+        //return the item which contains the user input
+        else {
+            return el.todo.toLowerCase().includes(inputText)
+        }
+    })
+    useEffect(()=>{
+        const local = localStorage.getItem('todos')
+        setTodos(JSON.parse(local))
+    })
+
+  return (
+    <div className={styles.content}>
+        <input type="text" onChange={inputHandler} placeholder='Search' />
+        <ul className='float-items'>
+            {datafiltered?.map(value=>{
+                return <li>
+                            <div className={styles.todo}>
+                                <div>
+                                    <h4>{value.todo}</h4>
+                                    <p>{value.description}</p>
+                                </div>
+                            </div>
+                        </li>
+            })}
+            
+        </ul>
+    </div>
+  )
+}
+
+export default Search

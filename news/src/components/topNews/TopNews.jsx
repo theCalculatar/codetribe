a173d748497f833f8f3babf91ddf68a5d0281278
theCalculatar <alpheusm13@gmail.com>
2024-08-10 17:09:48 +0200

    added api on top news

--- news/src/components/topNews/TopNews.jsx
+++ news/src/components/topNews/TopNews.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './topNews.module.css'
 import { LuDot } from "react-icons/lu";
+import axios from 'axios';
 
 function TopNews() {
+    const API_KEY = '7d098d91d1234b9393a9a4ca62806d8e'
+
+    const [latest, setLatest] = useState([])
+
+    const [news, setTopPart] = useState(null)
+    useEffect(()=>{
+        const getNews = async () =>{
+            try {
+                const newsApi = await axios.get(`https://newsapi.org/v2/everything?q=netflix&pageSize=1&sortBy=relevancy&from=2024-08-08&apiKey=${API_KEY}`)
+                    setLatest(newsApi.data.articles)
+                    setTopPart(newsApi.data.articles[0])
+            } catch (error) {
+                console.log(error)
+            }
+        } 
+        getNews()
+    },[])
   return (
     <div className={style.top_news}>
-        <img className={style.img_left} src="../public/me_pic.jpg" alt="img" />
+        <img className={style.img_left} src={news?.urlToImage} alt="img" />
         <div className={style.right}>
             <div className={style.source}>
-                <img src="../public/me_pic.jpg" alt="" />
-                <p>Netflix</p>
+                <img src={news?.urlToImage} alt="" />
+                <p>{news?.author}</p>
                 <LuDot/>
                 <p>12 minutes ago</p>
             </div>
-            <h1>Where To Watch 'John Wick: Chapter 4'</h1>
-            <p>There's been no official announcement regarding John Wick:
-                Chapter 4's streaming release. However, given it's a Lionsgate
-                film, John Wick: Chapter 4 will eventually be released on Starz,.</p>
+            <h1>{news?.title}</h1>
+            <p>{news?.description}</p>
             <div className={style.catg}>
-                <p >Movies</p>
+                <p >{news?.source.name}</p>
                 <LuDot/>
                 <p>4 min read</p>
             </div>
